0fac3168d42b2d63c65fa4e5f6482bf034edc9ef
Phil <cpmoviephil@gmail.com>
2020-11-10 11:16:19 +0800

    temp

--- android/app/src/main/kotlin/com/example/blueProject/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/blueProject/MainActivity.kt
@@ -1,6 +1,40 @@
 package com.example.blueProject
 
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger,"com.example.messages")
+                .setMethodCallHandler{call,result->
+
+                    if(call.method=="startService")
+                    {
+                        startService()
+                    }
+                }
+
+    }
+    lateinit var intent:Any
+
+    fun startService()
+    {
+        intent=Intent(this,AppService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent as Intent)
+        }else
+        {
+            startService(intent as Intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //stopService(intent as Intent)
+    }
 }
